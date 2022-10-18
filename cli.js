ac2d420f629bf5e15310e03a4576dc9a3097e748
Emma Goodrich <emmaliz@email.unc.edu>
2022-10-18 12:53:00 -0400

    fixed -j argument

--- cli.js
+++ cli.js
@@ -17,11 +17,7 @@ const timezone = moment.tz.guess()
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7');
 // Get the data from the request
 const data = await response.json();
-// Structure output
 
-// Cleanup and exit (if necessary)
-// Log the data onto STDOUT
-console.log(data);
 
 // Structure output
 // Use minimist to parse command line arguments
@@ -29,6 +25,11 @@ const args = minimist(process.argv.slice(2))
 // See what is stored in `args` by minimist
 console.log(args)
 // Default action
+if (args.j) {
+    console.log(data);
+    process.exit(0)
+}
+
 // Was the command called with `-h`?
 if ( args.h ) {
 // If yes, then log the help message onto STDOUT
@@ -53,6 +54,8 @@ if (days == 0) {
 } else {
   console.log("tomorrow.")
 }
+
+
 // Cleanup and exit (if necessary)
 
 
