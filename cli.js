bd5b5dd9b44c77d01958032f61c58adc9a66fa50
Emma Goodrich <emmaliz@email.unc.edu>
2022-10-18 13:34:16 -0400

    fixed url

--- cli.js
+++ cli.js
@@ -14,7 +14,7 @@ const timezone = moment.tz.guess()
 
 // Core function
 // Make a request
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m,relativehumidity_2m,precipitation,surface_pressure&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=7');
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude +  '&daily=precipitation_hours&current_weather=true&timezone=America%2FNew_York&past_days=7');
 // Get the data from the request
 const data = await response.json();
 
